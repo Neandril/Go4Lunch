e87bfbd2b858aebd8deae30fbb3fb05a83af1719
Rémy <remy.dalla@gmail.com>
2019-10-13 20:00:18 +0200

    Implementation of the Login Activity

---
+++ app/src/main/java/com/neandril/go4lunch/controllers/activities/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.neandril.go4lunch.controllers.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.neandril.go4lunch.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
