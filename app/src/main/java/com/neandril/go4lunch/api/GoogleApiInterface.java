20f1bffe7e003d1d510e57770062061d0944b248
Rémy DALLAVALLE <remy.dalla@gmail.com>
2019-11-04 16:03:59 +0100

    BuildConfig

--- app/src/main/java/com/neandril/go4lunch/api/GoogleApiInterface.java
+++ app/src/main/java/com/neandril/go4lunch/api/GoogleApiInterface.java
@@ -1,21 +1,24 @@
 package com.neandril.go4lunch.api;
 
+
+import com.neandril.go4lunch.BuildConfig;
 import com.neandril.go4lunch.models.details.Detail;
 import com.neandril.go4lunch.models.places.PlacesDetail;
 
-import io.reactivex.Observable;
 import retrofit2.Call;
 import retrofit2.http.GET;
 import retrofit2.http.Query;
 
 public interface GoogleApiInterface {
 
+    String key = BuildConfig.ApiKey;
+
     //TODO: Manage Api Key via gradle file
-    @GET("nearbysearch/json?sensor=true&radius=5000&type=restaurant&key=AIzaSyBkjaxczMoqyJzCQnRRIJgeJoubLGdSEK0")
+    @GET("nearbysearch/json?sensor=true&radius=5000&type=restaurant&key=" + key)
     Call<PlacesDetail> getNearbyPlaces(
             @Query("location") String location);
 
-    @GET("details/json?&key=AIzaSyBkjaxczMoqyJzCQnRRIJgeJoubLGdSEK0")
+    @GET("details/json?&key=" + key)
     Call<Detail> getDetailInfos(
             @Query("placeid") String placeid);
 }
